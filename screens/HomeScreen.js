65c28a6e3741a2efd1167255654ff7dc54b3b4c1
Joel Crawford <macbook@Crawford.local>
2023-04-27 12:12:18 +0300

    Location and reverse geocoding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -19,9 +19,11 @@ const HomeScreen = () => {
     const checkIfLocationIsEnabled = async () => {
         let enabled = await Location.hasServicesEnabledAsync();
 
+        console.log("ENABLED: ", enabled)
+
         if (!enabled) {
             //Give alert message to the user
-            Alert.alert('Location services are sot enabled', 'Please enable location button', [
+            Alert.alert('Location services are not enabled', 'Please enable location button', [
                 {
                     text: 'Cancel',
                     onPress: () => console.log('Cancel Pressed'),
@@ -33,9 +35,88 @@ const HomeScreen = () => {
             setLocationServicesEnabled(enabled)
         }
 
+    };
+    const getCurrentLocation = async () => {
 
-    }
-    const getCurrentLocation = () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        console.log("STATUS: ", status)
+
+        if (status !== "granted") {
+            Alert.alert(
+                "Permission denied",
+                "Allow the app to use the location services",
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel",
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") },
+                ],
+                { cancelable: false }
+            );
+        }
+
+        const { coords } = await Location.getCurrentPositionAsync();
+        console.log("CORDS:", coords)
+        if (coords) {
+            const { latitude, longitude } = coords;
+
+            //Reverse Geocode method
+            let response = await Location.reverseGeocodeAsync({
+                latitude,
+                longitude,
+            });
+
+            console.log("response ", response)
+
+            for (let item of response) {
+                let address = `${item.name} ${item.city} ${item.postalCode}`;
+                setDisplayCurrentAddress(address);
+            }
+        }
+        // let status = await Location.requestForegroundPermissionsAsync();
+
+        // console.log("STATUS: ", status)
+
+        // if (status !== "granted") {
+        //     Alert.alert('Permission denied', 'Allow the app to use the location services', [
+        //         {
+        //             text: 'Cancel',
+        //             onPress: () => console.log('Cancel Pressed'),
+        //             style: 'cancel',
+        //         },
+        //         {
+        //             text: 'OK', onPress: () => {
+
+        //             }
+        //         },
+        //     ],
+        //         { cancelable: false });
+        // };
+        // //if permission granted
+
+        // const { cords } = await Location.getCurrentPositionAsync();
+
+        // console.log("CORDS: ", cords)
+
+        // if (cords) {
+        //     const { latitude, longitude } = cords;
+
+        //     //Reverse Geocode method
+
+        //     let response = await Location.reverseGeocodeAsync({ latitude, longitude });
+
+
+
+        //     for (let item in response) {
+        //         let address = `${item.name} ${item.city} ${item.postalCode}`;
+        //         setDisplayCurrentAddress(address);
+        //     }
+        //     console.log("RESPONSE: ", response)
+
+        // }
 
     }
 
