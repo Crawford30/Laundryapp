ee91bf9ea3e538c08e0ab4066e07afd37d1e5904
Joel Crawford <macbook@Crawford.local>
2023-04-27 12:33:55 +0300

    Header view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, SafeAreaView, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import * as Location from 'expo-location'
+import { StyleSheet, Text, View, SafeAreaView, Alert, Pressable, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const HomeScreen = () => {
@@ -58,8 +59,9 @@ const HomeScreen = () => {
             );
         }
 
+        //if permission granted
         const { coords } = await Location.getCurrentPositionAsync();
-        console.log("CORDS:", coords)
+
         if (coords) {
             const { latitude, longitude } = coords;
 
@@ -69,54 +71,13 @@ const HomeScreen = () => {
                 longitude,
             });
 
-            console.log("response ", response)
-
+            //console.log("response ", response)
             for (let item of response) {
                 let address = `${item.name} ${item.city} ${item.postalCode}`;
                 setDisplayCurrentAddress(address);
             }
         }
-        // let status = await Location.requestForegroundPermissionsAsync();
 
-        // console.log("STATUS: ", status)
-
-        // if (status !== "granted") {
-        //     Alert.alert('Permission denied', 'Allow the app to use the location services', [
-        //         {
-        //             text: 'Cancel',
-        //             onPress: () => console.log('Cancel Pressed'),
-        //             style: 'cancel',
-        //         },
-        //         {
-        //             text: 'OK', onPress: () => {
-
-        //             }
-        //         },
-        //     ],
-        //         { cancelable: false });
-        // };
-        // //if permission granted
-
-        // const { cords } = await Location.getCurrentPositionAsync();
-
-        // console.log("CORDS: ", cords)
-
-        // if (cords) {
-        //     const { latitude, longitude } = cords;
-
-        //     //Reverse Geocode method
-
-        //     let response = await Location.reverseGeocodeAsync({ latitude, longitude });
-
-
-
-        //     for (let item in response) {
-        //         let address = `${item.name} ${item.city} ${item.postalCode}`;
-        //         setDisplayCurrentAddress(address);
-        //     }
-        //     console.log("RESPONSE: ", response)
-
-        // }
 
     }
 
@@ -124,7 +85,18 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView>
-            <Text>HomeScreen</Text>
+            <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+                <MaterialIcons name="location-on" size={30} color="#fd5c63" />
+                <View>
+                    <Text style={{ fontSize: 18, fontWeight: "600" }}>Home</Text>
+                    <Text>{displayCurrentAddress}</Text>
+                </View>
+
+            </View>
+            <Pressable style={{ marginLeft: "auto", marginRight: 7 }}>
+                <Image style={{ width: 40, height: 40, borderRadius: 20 }} source={{ uri: "https://lh3.googleusercontent.com/ogw/AOLn63E6EXErelWxXSUFxUiORVdmlsp-qjQWBmx6iCtqjg=s64-c-mo" }}></Image>
+            </Pressable>
+
         </SafeAreaView>
     )
 }
